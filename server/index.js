856db8022421dc39cdfda7e7179f341a50306963
sarojkumar2002 <165543008+sarojkumar2002@users.noreply.github.com>
2025-09-26 23:18:12 +0530

    Refactor CORS configuration and clean up console log formatting

--- server/index.js
+++ server/index.js
@@ -16,25 +16,22 @@ connectDB();
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-console.log("Port number is : " , PORT);
+console.log("Port number is : ", PORT);
 
 // default middleware
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}));
- 
+app.use(cors());
+
 // apis
 app.use("/api/v1/media", mediaRoute);
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/course", courseRoute);
 app.use("/api/v1/purchase", purchaseRoute);
 app.use("/api/v1/progress", courseProgressRoute);
- 
- 
+
+
 app.listen(PORT, () => {
     console.log(`Server listen at port ${PORT}`);
 })
